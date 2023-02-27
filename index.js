667851174f870d5424d1500ca95ab7eb09030f77
abdulaz1zyoqubov777 <abdulazizyoqubov84@gmail.com>
2023-02-27 14:15:25 +0500

    homework-3

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 
-let x=12
 
-console.log(x);
+let num="-1";
 
+if (num<"0"){
+console.log("manfiy");
+}
+else if (num <=1 && num >0){
+    console.log("musbat");
+}
+else if (num <=0 && num >-1){
+    console.log("betaraf");}
