909e45ec0f6bfcac43b7410054dfaf92b5d427d4
abdulaz1zyoqubov777 <abdulazizyoqubov84@gmail.com>
2023-03-01 15:27:11 +0500

    if,alse

--- index.js
+++ index.js
@@ -2,11 +2,27 @@
 
 
 
-    let a=1
-    b=2
-    if (a+b<4){
-        console.log("below");
-    }
-    else {
-        console.log("over");
-    }
+   // let a=1
+   // b=2
+    //if (a+b<4){
+      //  console.log("below");}
+    //else {
+      //  console.log("over");}
+        
+
+
+     // if ('0') {
+      //  alert('Hello');}
+
+
+    //   let name='ECMASscript';
+    //   if (name === 'ECMASscript') console.log('right');
+    //   else console.log('you dont know ECMASscript');
+
+
+    // let message= 'No login';
+    // message == 'Employee'?console.log('hello'):0
+    //  message == 'Director'?console.log('Greetings'):0
+    //  message == 'No login'?console.log(''):0
+
+    
