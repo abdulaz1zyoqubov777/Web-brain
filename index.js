3dccb4cebbf5a8971557c0442e7b12b666a6de11
abdulaz1zyoqubov777 <abdulazizyoqubov84@gmail.com>
2023-03-03 19:57:59 +0500

    do while,switch

--- index.js
+++ index.js
@@ -2,166 +2,52 @@
 
 
 
-   
-
-    // let a=1,b=1;
-    
-    // console.log(++a);=>2 chunki a ni qiymati oldin 1ga oshadi song erkanga chiqadi
-    // console.log(b++);->1 chunki b ni qiymati erkanga chiqadi keyin 1 ga oshadi
-    
-    
-    // let a=2;
-    // console.log(a);
-    // let x= 1 + (a *= 2)
-    // console.log(x);
-//"" +1+0
-
-    // let a="1",b=1,c=0;
-    // console.log(+a + b + c);
-    // let a="2",b=1,c=0;
-    // console.log(+a - b + c);
-
-    // let a=true,b=false;
-    // console.log(a+b);=>1 true ni 1 deb oldi , falseni esa 0 deb qabul qildi
-
-    // let a=6, b="3";
-    // console.log(a / +b);=>2 b strinni oldida arifmetik amal bajarib raqam qilindi
-
-    // let a=2,b=3;
-    // console.log(+a * +b);=>6
-
-
-    // let a=4, b=5, c="px";
-    // console.log(a + b + c);=>9px
-    // console.log(a + b + +c);=>NaN raqam bolmagan malumotni ustida arifmetik amal bajarildi
-
-
-    // let a='$',b=4,c=5;
-    // console.log(a + b + c);=>$45 a strin raqam emas raqamga aylantirib bolmaydi
-    // console.log(+a + b + c);=>NaN raqam bolmagan malumotni ustida arifmetik amal bajarildi
-
-    // let a="4",b=2;
-    // console.log(+a - b);=>2 a strinini oldiga arifmetik amal bajarib raqam qildik
-
-    // let a="-9", b=5;
-    // console.log(+a + b);=> -4 a strinini oldiga arifmetik amal bajarib raqam qildik
-
-    // let a="4px", b=2;
-    // console.log(parseInt(a)-b);=>2 a strinni parseInt qilib butun qismini olindi yani raqam bolgan joyini ozini
-    // console.log(Number(a)-b);=>NaN raqam bolmagan malumotni raqamga otgazib bolmaydi
-    // console.log(+a - b);=>NaN raqam bolmagan malumotni ustida amal bajarib bolmaydi
-
-//   let a=null, b=1;
-//   console.log(a + b);=>1  elon qilingan qiymat biriktirilgan oxgaruvchi kk bolmay qolsa ochirib qoyish mumkin
-
-// let a=undefined, b=1;
-// console.log(a + b);=> NaN ozgaruvchi elon qilingan lkn qiymat yoq
-
-
-// let a=5,b=4;
-// console.log(a > b);=> true 5 son 4 dan katta
-
-// let a='apple', b='pineaple';
-// console.log(a > b);=>false harflarning ozini raqami bor
-
-// let a=undefined, b=null;
-// console.log(a == b);=>true qiymatini tekshirdi halos
-// console.log(a === b);=> false qiymatini ham data type ni ham tekshirad
-
-// let a=null, b="\n0\n"
-// console.log(a == b);//=> false qiymati birhil emas
-// console.log(a === b);//=> false qiymati ham data type ham birhil emas
-// console.log(a === +b);=> false qiymati ham data type ham harhil
-
-
-// let oraliq=12;
-// if (oraliq>=9){
-// console.log("qoniqarli");
-// }
-// else if(oraliq>=12){
-//     console.log("alo");
-// }
-// else {
-//    console.log("qoniqarsiz"); 
-// }
-
-//?->if , :->else
-
-
-// let age=70;
-
-// console.log(age>60 ? "togri":"notogri");
-
-
-// console.log(0 || null || 1);
-// console.log(0 && 1);
-
-//nullish->null, undefined
-
-
-//console.log(1 ?? null ?? 0 );
-
-//for(begin, condition,step)
-
-// for(i=7 ; i>=1 ; i--){
-// console.log(i);
-// }
-
-
-// let oraliq=12;
-// if (oraliq>=9){
-// console.log("qoniqarli");
-// }
-//  else if (oraliq>=12){
-//     console.log("alo");
-// }
-// else {
-//     console.log("qoniqarsiz");
-// }
-
-
-// ?>->if , :->else
-
-
-// let age=70;
-// console.log(age>60 ? "togri":"notogri");
-
-// console.log(0 || 1 || undefined);
-// console.log(false && null);
-
-//nullish-> null, undefined
-
-// console.log(undefined ?? true);
-
-//for(begin , condition,step)
-
-// for(i=7;i>=1;i--){
-//     console.log(i);
-// }
 
 
 
-// let oraliq=12;
-// if (oraliq==12){
-// console.log("alo");
-// }
-// else if (oraliq>=9){
-//     console.log("qoniqarli");
-// }
-// else{
-//     console.log("qoniqarsiz");
-// }
 
-//?->if , :->else
+//do while
+let res = 2;
+while (res) {
+    console.log(res);
+    res--;
 
-// let age=70;
-// console.log(age>60 ? "togri":"notogri");
+}
+
+let j = 1;
+do {
+    console.log(j);
+    j--
+} while (j);
 
 
-// console.log(1 || null);
-// console.log(0 && null);
+//console.log(new Date().getDay());
 
-//nullish->null, undefined
+let time = new Date().getDay();
+switch (time) {
+    case  0:
+        console.log("yakshanba");
+        break;
+    case  1:
+        console.log("dushanba");
+        break;
+    case  2:
+        console.log("seshanba");
+        break;
+    case  3:
+        console.log("chorshanba");
+        break
+    case  4:
+        console.log("payshanba");
+        break
+    case  5:
+        console.log("juma");
+        break
+    case  6:
+        console.log("shanba");
+        break
 
- console.log(false ?? true);
-console.log(null ?? true);
+
+    default:console.log(404);
+
+}
