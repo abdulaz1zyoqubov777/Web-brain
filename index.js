2ea51e3b93fa1ab7d667d2b3b3659d47a2539f63
abdulaz1zyoqubov777 <abdulazizyoqubov84@gmail.com>
2023-03-04 15:40:08 +0500

    switch

--- index.js
+++ index.js
@@ -7,47 +7,93 @@
 
 
 //do while
-let res = 2;
-while (res) {
-    console.log(res);
-    res--;
+// let res = 2;
+// while (res) {
+//     console.log(res);
+//     res--;
 
-}
+// }
 
-let j = 1;
-do {
-    console.log(j);
-    j--
-} while (j);
+// let j = 1;
+// do {
+//     console.log(j);
+//     j--
+// } while (j);
 
 
 //console.log(new Date().getDay());
 
-let time = new Date().getDay();
-switch (time) {
-    case  0:
+// let time = new Date().getDay();
+// switch (time) {
+//     case  0:
+//         console.log("yakshanba");
+//         break;
+//     case  1:
+//         console.log("dushanba");
+//         break;
+//     case  2:
+//         console.log("seshanba");
+//         break;
+//     case  3:
+//         console.log("chorshanba");
+//         break
+//     case  4:
+//         console.log("payshanba");
+//         break
+//     case  5:
+//         console.log("juma");
+//         break
+//     case  6:
+//         console.log("shanba");
+//         break
+
+
+//     default:console.log(404);
+
+// }
+
+// do while 
+
+// let a=0;
+// while (a) {
+//  console.log("day",a);
+//  a--  
+// }
+
+// let b=1;
+// do {
+//     console.log("week",b);
+//     b--
+// } while (b);
+
+
+//switch 
+
+let week = 7;
+
+switch (week) {
+    case 0:
         console.log("yakshanba");
         break;
-    case  1:
+    case 1:
         console.log("dushanba");
         break;
-    case  2:
+    case 2:
         console.log("seshanba");
         break;
-    case  3:
+    case 2:
         console.log("chorshanba");
-        break
-    case  4:
+        break;
+    case 3:
         console.log("payshanba");
-        break
-    case  5:
+        break;
+    case 4:
         console.log("juma");
-        break
-    case  6:
+        break;
+    case 5:
         console.log("shanba");
         break
-
-
-    default:console.log(404);
-
+    default:
+        console.log(404);
+        break;
 }
