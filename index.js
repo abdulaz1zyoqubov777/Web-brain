946a6c33d0831f75d3f6abc1b791e68c4b092bba
abdulaz1zyoqubov777 <abdulazizyoqubov84@gmail.com>
2023-03-07 15:31:52 +0500

    function

--- index.js
+++ index.js
@@ -6,94 +6,87 @@
 
 
 
-//do while
-// let res = 2;
-// while (res) {
-//     console.log(res);
-//     res--;
-
-// }
-
-// let j = 1;
-// do {
-//     console.log(j);
-//     j--
-// } while (j);
-
-
-//console.log(new Date().getDay());
-
-// let time = new Date().getDay();
-// switch (time) {
-//     case  0:
-//         console.log("yakshanba");
-//         break;
-//     case  1:
-//         console.log("dushanba");
-//         break;
-//     case  2:
-//         console.log("seshanba");
-//         break;
-//     case  3:
-//         console.log("chorshanba");
-//         break
-//     case  4:
-//         console.log("payshanba");
-//         break
-//     case  5:
-//         console.log("juma");
-//         break
-//     case  6:
-//         console.log("shanba");
-//         break
-
-
-//     default:console.log(404);
-
-// }
-
-// do while 
-
-// let a=0;
-// while (a) {
-//  console.log("day",a);
-//  a--  
-// }
-
-// let b=1;
-// do {
-//     console.log("week",b);
-//     b--
-// } while (b);
-
 
 //switch 
 
-let week = 7;
+// let week = 7;
 
-switch (week) {
-    case 0:
-        console.log("yakshanba");
-        break;
-    case 1:
-        console.log("dushanba");
-        break;
-    case 2:
-        console.log("seshanba");
-        break;
-    case 2:
-        console.log("chorshanba");
-        break;
-    case 3:
-        console.log("payshanba");
-        break;
-    case 4:
-        console.log("juma");
-        break;
-    case 5:
-        console.log("shanba");
-        break
-    default:
-        console.log(404);
-        break;
-}
+// switch (week) {
+//     case 0:
+//         console.log("yakshanba");
+//         break;
+//     case 1:
+//         console.log("dushanba");
+//         break;
+//     case 2:
+//         console.log("seshanba");
+//         break;
+//     case 2:
+//         console.log("chorshanba");
+//         break;
+//     case 3:
+//         console.log("payshanba");
+//         break;
+//     case 4:
+//         console.log("juma");
+//         break;
+//     case 5:
+//         console.log("shanba");
+//         break;
+//     default:
+//         console.log(404);
+//         break;
+// }
+
+
+// for(a=2;a<=10;a++){
+//     for(b=1;b<=10;b++)
+//     console.log(a,"*",b,"=",a*b);
+//     console.log("==============");
+// }
+
+
+
+// functions
+
+
+
+
+
+
+//==================
+
+function telegram(){
+    let age=18
+    if(age>18){
+        return true;
+    }
+    else{
+        return "Did you parent allow you?";
+    }
+};
+console.log(telegram());
+
+//===================
+
+
+// min(2,5)==2
+// min(3,-1)==-1
+// min(1,1)==1
+
+// function wrap() {
+//     console.log(Math.min(2, 5));
+//     console.log(Math.min(3, -1));
+//     console.log(Math.min(1, 1));
+//   }
+//  wrap();
+ 
+//================
+// function min(a, b){
+//     if (a<b) return a;
+//     else return b;
+
+// }
+// console.log(min(3, -1));
+
+
