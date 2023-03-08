5bbc93f41b5b13cebf29a6dac2ea14455e9f74cb
abdulaz1zyoqubov777 <abdulazizyoqubov84@gmail.com>
2023-03-08 17:33:36 +0500

    object one line for each action

--- index.js
+++ index.js
@@ -7,86 +7,19 @@
 
 
 
-//switch 
 
-// let week = 7;
 
-// switch (week) {
-//     case 0:
-//         console.log("yakshanba");
-//         break;
-//     case 1:
-//         console.log("dushanba");
-//         break;
-//     case 2:
-//         console.log("seshanba");
-//         break;
-//     case 2:
-//         console.log("chorshanba");
-//         break;
-//     case 3:
-//         console.log("payshanba");
-//         break;
-//     case 4:
-//         console.log("juma");
-//         break;
-//     case 5:
-//         console.log("shanba");
-//         break;
-//     default:
-//         console.log(404);
-//         break;
+// let name ="title"
+// let user = {
+//     name:"John",
+//     surname:"Smith",
+//     [name]:"Pate",
+//     names:""
 // }
-
-
-// for(a=2;a<=10;a++){
-//     for(b=1;b<=10;b++)
-//     console.log(a,"*",b,"=",a*b);
-//     console.log("==============");
-// }
-
-
-
-// functions
-
-
-
-
-
-
-//==================
-
-function telegram(){
-    let age=18
-    if(age>18){
-        return true;
-    }
-    else{
-        return "Did you parent allow you?";
-    }
-};
-console.log(telegram());
-
-//===================
-
-
-// min(2,5)==2
-// min(3,-1)==-1
-// min(1,1)==1
-
-// function wrap() {
-//     console.log(Math.min(2, 5));
-//     console.log(Math.min(3, -1));
-//     console.log(Math.min(1, 1));
-//   }
-//  wrap();
  
-//================
-// function min(a, b){
-//     if (a<b) return a;
-//     else return b;
 
-// }
-// console.log(min(3, -1));
-
+// console.log(user.name);
+// console.log(user.surname);
+// console.log(user[name]);
+// console.log(delete user.name);
 
