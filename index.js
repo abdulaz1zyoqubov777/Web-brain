a977d81cd478b59007182cc167d55349296012c6
abdulaz1zyoqubov777 <abdulazizyoqubov84@gmail.com>
2023-03-08 17:35:52 +0500

    Object1

--- index.js
+++ index.js
@@ -9,17 +9,17 @@
 
 
 
-// let name ="title"
-// let user = {
-//     name:"John",
-//     surname:"Smith",
-//     [name]:"Pate",
-//     names:""
-// }
+let name ="title"
+let user = {
+    name:"John",
+    surname:"Smith",
+    [name]:"Pate",
+    names:""
+}
  
 
-// console.log(user.name);
-// console.log(user.surname);
-// console.log(user[name]);
-// console.log(delete user.name);
+console.log(user.name);
+console.log(user.surname);
+console.log(user[name]);
+console.log(delete user.name);
 
