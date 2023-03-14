da109a2b239205565b3b364ca11db48f08455f0e
abdulaz1zyoqubov777 <abdulazizyoqubov84@gmail.com>
2023-03-14 15:32:53 +0500

    Numbers

--- index.js
+++ index.js
@@ -1,30 +1,102 @@
 
+// let salaries = {
+//        John:100,
+//        Ann:160,
+//        Pete:130,
+//    }
+//    let wrap=0;
+//    for(let i in salaries){
+//        wrap=wrap+salaries[i]
+//    }
+//    console.log(wrap);
+
+//    let name ="title"
+// let user = {
+//     name:"John",
+//     surname:"Smith",
+//     [name]:"Pate",
+//     names:""
+// }
+
+// console.log("name" in user);+
+
+// console.log(user.name); 
+// console.log(user.surname);
+// console.log(user[name]);
+// console.log(delete user.name);
+
+// function getName(){
+//     this.name="University"
+//     console.log(this);
+// }
+// console.log(new getName().name);
+
+
+// function getName(name){
+    
+//     console.log(new.target);
+// }
+// new getName("hello")
+
+// let obj={
+    
+//     child:{
+//         childname:"childrens"
+//     }
+// }
+// console.log(obj);
+// obj={...obj.child}
+// console.log(obj);
+
+//==================================================================
+
+// 
+//==========================vazifa
+let title=42;
+
+console.log(title.toString(2));
+console.log(title.toString());
+
+let name={
+    title:5,
+    wrap:"2 ta bir,1 ta nol qatnashgan"
+}
+console.log(name.title.toString(2));
+console.log(name.wrap);
 
 
 
+     //==============================  
+
+        
+
+      
+
+      let obj={
+         a:Math.min(2, 3),
+         b:Math.max(2, 3)
+     }
+     const {a, b}=obj;
+     console.log(Math.abs(a - b));
+
+             
+        
+
+        // //Math ning truncs vs floor vs ceil, parseInt lar bn farqini tushintirib bering
+
+         // console.log(Math.trunc(3.2));//=>(3)nima bolishidan qatiy nazar butun qismini ovoladi kasr qismini tashlab yuboradi sonni musbat manfiysiga qaramaydi 
+         // console.log(Math.floor(-3.23));//=>(-4)sonning kasr qismini tashlab yuboradi {manfiy sonda 1 ga kamaytirib yuboradi, musbat sonda 1 ga oshmaydi}
+         // console.log(Math.ceil(-3.2));//=>(-3)raqamning kasr qismi bolsa tashlab yuboradi va birga oshirib beradi
+         // console.log(parseInt(3.2));//=>sonning kasr qismi bolsa tashlab yuborib butun qismini ozini olib beradi
+
+      //   let obj={
+      //       a:Math.min(1, 50),
+      //       b:Math.max(1, 50)
+      //   }
+      //   console.log(Math.random()*50);
+
+      
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-let salaries = {
-       John:100,
-       Ann:160,
-       Pete:130,
-   }
-   let wrap=0;
-   for(let i in salaries){
-       wrap=wrap+salaries[i]
-   }
-   console.log(wrap);
-
+      
+        
