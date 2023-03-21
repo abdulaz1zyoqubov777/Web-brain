6e3519accd510be032e68ce9d91042f0d7108440
abdulaz1zyoqubov777 <abdulazizyoqubov84@gmail.com>
2023-03-21 21:47:41 +0500

    Arrays

--- vazifa.js
+++ vazifa.js
@@ -1,16 +1,33 @@
 
+// const name = (str) => {
+//     let res = "";
+//     for (key in str) {
+//       if (str[key - 1] === " " || str[key - 1] === undefined)
+//         res += str[key].toUpperCase();
+//       else res += str[key];
+//     }
+//     return res;
+//   };
+//   console.log(name("webbrain academy it center"));
 
 
-//=======================================
 
 
-const name = (str) => {
-    let res = "";
-    for (key in str) {
-      if (str[key - 1] === " " || str[key - 1] === undefined)
-        res += str[key].toUpperCase();
-      else res += str[key];
-    }
-    return res;
-  };
-  console.log(name("webbrain academy it center"));
+
+//==Array====
+
+
+// let ar=[1,2,3,"hi",true,function(){},null,undefined];
+
+// console.log(ar[6]);//6 indexni korstadi
+// console.log(ar.length);//index uzunligi
+// console.log(ar[ar.length-1]);//eng ohirgi indexni korsatadi
+
+//================================================================
+
+// let  ar=new Array(5, 1);//element yasash un
+
+// console.log(ar);
+// console.log(ar.length);
+
+
