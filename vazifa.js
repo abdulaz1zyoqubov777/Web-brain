18d7e411d8a3f73ec751e185abd389fffec0d3bf
abdulaz1zyoqubov777 <abdulazizyoqubov84@gmail.com>
2023-03-22 23:04:32 +0500

    the sum of the numbers in the array

--- vazifa.js
+++ vazifa.js
@@ -31,3 +31,59 @@
 // console.log(ar.length);
 
 
+//===============array1 homework
+
+//   function sumAr(){
+//     let Ar=[1,2,3];
+//     let sum=0;
+//     for(i=0;i<Ar.length;i=i+1){
+//         sum=sum+Ar[i];
+//     }
+//     return sum;
+//   }
+//   console.log(sumAr());
+
+
+
+// function compareArrays(arr1, arr2) {
+
+    
+//     if(arr1.length != arr2.length) {
+//         return false;
+//     } 
+//     else { 
+//         let result = false;
+        
+       
+//         for(let i=0; i<arr1.length; i++) {
+
+//             if(arr1[i] != arr2[i]) {
+//                 return false;
+//             }
+//             else {
+//                 result = true;
+//             }
+
+//         }
+//         return result;
+        
+//   }
+
+// }
+
+// const array1 = [1, 3, 5, 8];
+// const array2 = [1, 3, 5, 5];
+
+// const result = compareArrays(array1, array2);
+
+// if(result) {
+//     console.log('true');
+// }
+// else {
+//     console.log('false');
+// }
+
+//==============
+
+
+
