805dce984f2ac622de594bf0325c7f1a6cc6fa69
abdulaz1zyoqubov777 <abdulazizyoqubov84@gmail.com>
2023-04-26 22:47:45 +0500

    dom1

---
+++ dom.js
@@ -0,0 +1,25 @@
+
+let ul = document.getElementsByTagName("ul");
+//console.log(ul);
+console.log(ul.children);
+console.log(ul.childNodes);
+//===========
+
+let parent = document.getElementsByClassName("parent")
+
+let id = document.getElementById("bir")
+
+//===================
+
+let qr = document.querySelector("ul")
+
+let qr2 = document.querySelectorAll("#bir")
+console.log(qr2);
+console.log(qr);
+
+//==============================
+
+let qur = document.querySelector("#bir");
+bir.innerHTML="<h1>Hey</h1>"
+
+qur.innerText="<h1>Hey</h1>"
